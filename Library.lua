98d7b0e28bb8b8690b21048e74fcb5453c45e768
top10realbilly <126090877+top10realbilly@users.noreply.github.com>
2024-03-08 20:30:53 +0100

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -33,7 +33,7 @@ local Library = {
     MainColor = Color3.fromRGB(28, 28, 28);
     BackgroundColor = Color3.fromRGB(20, 20, 20);
     AccentColor = Color3.fromRGB(17, 133, 168);
-    OutlineColor = Color3.fromRGB(17, 133, 168);
+    OutlineColor = Color3.fromRGB(20, 20, 20);
     RiskColor = Color3.fromRGB(255, 50, 50),
 
     Black = Color3.new(0, 0, 0);
